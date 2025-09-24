2fa03f1747e45c7a238c350f9b5186be8d0471f3
aadithyamanoj <aadithyasmanoj@gmail.com>
2025-09-23 18:36:10 -0700

    removed old versions

--- obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /app/verilator/share/verilator/include/verilated_threads.cpp \
- /app/verilator/share/verilator/include/verilatedos.h \
- /app/verilator/share/verilator/include/verilated_threads.h \
- /app/verilator/share/verilator/include/verilated.h \
- /app/verilator/share/verilator/include/verilated_config.h \
- /app/verilator/share/verilator/include/verilated_types.h \
- /app/verilator/share/verilator/include/verilated_funcs.h
--- test.c
+++
@@ -1,9 +0,0 @@
-#include "libmc.h"
-
-char dummy_string[128] = "This is just here to reassure you elftohex is working";
-
-// This is just here to make the linker happy for lab2/3
-int main() {
-    printf("Hi there %d\n", atoi("-55"));
-}
-
